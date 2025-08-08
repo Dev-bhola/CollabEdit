e36f409d62850f60673facc16a046a18f0bf43fb
Dev <devbhola1902@gmail.com>
2025-08-09 00:39:49 +0530

    deployed frontend

--- Backend/config/corsOptions.js
+++ Backend/config/corsOptions.js
@@ -1,5 +1,8 @@
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN,
+  origin: [
+    "http://localhost:5173",
+    "https://collab-edit-mci7x4gdv-dev-bholas-projects.vercel.app",
+  ],
   methods: ["GET", "POST"],
   credentials: true,
 };
